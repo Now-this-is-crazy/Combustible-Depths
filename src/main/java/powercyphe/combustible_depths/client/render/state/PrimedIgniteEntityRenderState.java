f5ea68580a5108ce82dc3549566128f4dcf6321d
Powercyphe <136335336+Powercyphe@users.noreply.github.com>
2026-03-27 23:52:51 +0100

    v1c

--- src/main/java/powercyphe/combustible_depths/client/render/state/PrimedIgniteEntityRenderState.java
+++ src/main/java/powercyphe/combustible_depths/client/render/state/PrimedIgniteEntityRenderState.java
@@ -1,12 +1,16 @@
 package powercyphe.combustible_depths.client.render.state;
 
+import net.minecraft.client.renderer.block.BlockModelRenderState;
+import net.minecraft.client.renderer.block.MovingBlockRenderState;
+import net.minecraft.client.renderer.block.dispatch.BlockStateModel;
 import net.minecraft.client.renderer.entity.state.EntityRenderState;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import powercyphe.combustible_depths.common.registry.CDBlocks;
 
 public class PrimedIgniteEntityRenderState extends EntityRenderState {
-    public BlockState blockState = CDBlocks.IGNITE.defaultBlockState();
+    public BlockModelRenderState blockState = new BlockModelRenderState();
+    // public BlockState blockState = CDBlocks.IGNITE.defaultBlockState();
 
     public float fuseProgress = 0F;
     public Vec3 shakiness = Vec3.ZERO;
